11d4e473be5f113968f0f25546195230940a0150
niamtokik <mk@steepath.eu>
2017-04-25 13:55:26 +0200

    Makefile automation, add new interface to iovec.h, writing some test for iovec.

--- c_src/iovec.h
+++ c_src/iovec.h
@@ -22,3 +22,27 @@ void iovec_push(struct iovec *, void *, size_t);
 struct tuple *iovec_tuple(void *, size_t, void *, size_t);
 struct tuple *iovec_tuples(struct tuple *, struct tuple *);
 struct iovec *tuple_to_iovec(struct tuple *);
+
+/** \brief new interfaces
+ *
+ * create a datastructure on stack or heap.
+ *   iovec *iovec_create(int, int);
+ *   tuple *tuple_create(int, int);
+ *
+ * datastructure initialization, agnostic interface
+ * can use heap or stack. set by default all to NULL,
+ * clean all memory:
+ *   void iovec_init(struct iovec*);
+ *   void tuple_init(struct tuple*);
+ *   void iovecs_init(struct iovec*, size_t);
+ *   void tuples_init(struct tuple*, size_t);
+ *
+ * set value.
+ *   void iovec_set(struct iovec*, void *, size_t);
+ *   void tuple_set(struct tuple*, int, void *, size_t);
+ *   iovec *tuple_get(struct tuple *, int);
+ *
+ * convert iovec/tuple datastructure structure
+ *   struct iovec *tuple_to_iovec(struct tuple*);
+ *   struct tuple *iovec_to_tuple(struct iovec*);
+ */
--- c_src/iovec_test.c
+++ c_src/iovec_test.c
@@ -2,30 +2,71 @@
  * 
  */
 
-#include <stdio.h>
 #include <atf-c.h>
 #include "iovec.h"
 
-ATF_TC(iovec);
-ATF_TC_HEAD(iovec, tc) {
-  atf_tc_set_md_var(tc, "descr", "iovec library test");
+/* iovec_init unit test */
+ATF_TC(iovec_init);
+
+ATF_TC_HEAD(iovec_init, tc) {
+  atf_tc_set_md_var(tc, "descr", "iovec_init function unit testing");
 }
-ATF_TC_BODY(iovec, tc) {
+
+ATF_TC_BODY(iovec_init, tc) {
+  struct iovec *io = iovec_init();
+  ATF_CHECK_EQ(io->iov_base, 0);
+  ATF_CHECK_EQ(io->iov_len, 0);
+}
+
+/* iovec_push unit test */
+ATF_TC(iovec_push);
+
+ATF_TC_HEAD(iovec_push, tc) {
+  atf_tc_set_md_var(tc, "descr", "iovec_push function unit testing");
+}
+ATF_TC_BODY(iovec_push, tc) {
   ATF_CHECK_EQ(0, 0 + 0);
   ATF_CHECK_EQ(2, 1 + 1);
 }
 
-ATF_TC(iovec2);
-ATF_TC_HEAD(iovec2, tc) {
-  atf_tc_set_md_var(tc, "descr", "iovec library test");
+/* iovec_tuple unit test */
+ATF_TC(iovec_tuple);
+
+ATF_TC_HEAD(iovec_tuple, tc) {
+  atf_tc_set_md_var(tc, "descr", "iovec_tuple function unit testing");
 }
-ATF_TC_BODY(iovec2, tc) {
+ATF_TC_BODY(iovec_tuple, tc) {
   ATF_CHECK_EQ(0, 0 + 0);
   ATF_CHECK_EQ(2, 1 + 1);
 }
 
+/* iovec_push unit test */
+ATF_TC(iovec_tuples);
+
+ATF_TC_HEAD(iovec_tuples, tc) {
+  atf_tc_set_md_var(tc, "descr", "iovec_tuples function unit testing");
+}
+ATF_TC_BODY(iovec_tuples, tc) {
+  ATF_CHECK_EQ(0, 0 + 0);
+  ATF_CHECK_EQ(2, 1 + 1);
+}
+
+/* iovec_push unit test */
+ATF_TC(tuple_to_iovec);
+
+ATF_TC_HEAD(tuple_to_iovec, tc) {
+  atf_tc_set_md_var(tc, "descr", "tuple_to_iovec function unit testing");
+}
+ATF_TC_BODY(tuple_to_iovec, tc) {
+  ATF_CHECK_EQ(0, 0 + 0);
+  ATF_CHECK_EQ(2, 1 + 1);
+}
+
+/* main ATF functions */
 ATF_TP_ADD_TCS(tp){
-  ATF_TP_ADD_TC(tp, iovec);
-  ATF_TP_ADD_TC(tp, iovec2);
+  ATF_TP_ADD_TC(tp, iovec_init);
+  ATF_TP_ADD_TC(tp, iovec_tuple);
+  ATF_TP_ADD_TC(tp, iovec_tuples);
+  ATF_TP_ADD_TC(tp, tuple_to_iovec);
   return atf_no_error();
 }         
